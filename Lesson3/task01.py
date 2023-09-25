fa24e99250fec00eaa27015d41c1f459596eded3
HenkSeed <adaner@inbox.ru>
2023-09-25 16:26:50 +0700

    Game Cross-Zero

--- Lesson3/task01.py
+++ Lesson3/task01.py
@@ -1,73 +1,64 @@
-""" Условие задачи
-● Контекст
-Вероятнее всего, вы с детства знакомы с этой игрой. Пришло 
-время реализовать её. Два игрока по очереди ставят крестики 
-и нолики на игровое поле. Игра завершается когда кто-то 
-победил, либо наступила ничья, либо игроки отказались 
-играть.
-
-● Задача
-Написать игру в “Крестики-нолики”. Можете использовать 
-любые парадигмы, которые посчитаете наиболее 
-подходящими. Можете реализовать доску как угодно - как 
-одномерный массив или двумерный массив (массив массивов). 
-Можете использовать как правила, так и хардкод, на своё 
-усмотрение. Главное, чтобы в игру можно было поиграть через 
-терминал с вашего компьютера. 
-"""
-# Создайте программу для игры в "Крестики-нолики".
-
-import sys
-
-# all_field = ['00', '01', '02', '10', '11', '12', '20', '21', '22']
-all_field = {
-    0: '⚐ ', 1: '⚐ ', 2: '⚐ ', 3: '⚐ ', 4: '⚐ ', 5: '⚐ ', 6: '⚐ ', 7: '⚐ ', 8: '⚐ '
-}
-
-indexes = {
-    '00': 0, '01': 1, '02': 2, '10': 3, '11': 4, '12': 5, '20': 6, '21': 7, '22': 8
-}
+board = list(range(1, 10))  # Генерируем номера клеток поля
 
 
-def prnt_fields(field):  # Вывод игрового поля
-    print(field[0], field[1], field[2])
-    print(field[3], field[4], field[5])
-    print(field[6], field[7], field[8])
+def draw_board(board):  # Рисуем игровое поле
+    print("-" * 13)
+    for i in range(3):
+        print("|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|")
+        print("-" * 13)
 
 
-def x_move():
-    x_turn = input('Ход "крестиков". Введите код поля:')
-    if x_turn not in indexes:
-        return False
-    all_field[indexes[x_turn]] = 'X '
-    indexes.pop(x_turn)  # Удаляем поле из словаря
-    prnt_fields(all_field)
-    return True
+def take_input(player_token):  # Обрабатываем ход игрока
+    valid = False
+    while not valid:  # Пока не сделаем ход без ошибок
+        player_answer = input("Куда поставим " + player_token+"? ")
+        try:
+            player_answer = int(player_answer)
+        except:
+            print("Некорректный ввод. Вы уверены, что ввели число?")
+            continue
+        if player_answer >= 1 and player_answer <= 9:  # Если введено число от 1 до 9
+            if (str(board[player_answer-1]) not in "XO"):  # Если выбранная клетка не занята
+                # То заносим в клетку X или O
+                board[player_answer-1] = player_token
+                valid = True  # Признак завершения хода
+            else:
+                print("Эта клеточка уже занята")
+        else:
+            print("Некорректный ввод. Введите число от 1 до 9 чтобы походить.")
 
 
-print('Нумерация полей:')
-for i in range(3):
-    for j in range(3):
-        print(str(i) + str(j), end=' ')
-    print('')
-
-try:
-    if not x_move():
-        sys.exit()
-
-except:
-    print('Невозможно !')
-
-else:
-    o_turn = input('Ход "ноликов". Введите код поля:')
-    all_field[indexes[o_turn]] = 'O '
-    prnt_fields(all_field)
-
-# x_turn = input('Ход "крестиков". Введите код поля:')
-# all_field[indexes[x_turn]] = 'X '
-# indexes.pop(x_turn) # Удаляем поле из словаря
-# prnt_fields(all_field)
+def check_win(board):  # Набор выигрышных вариантов
+    win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6),
+                 (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+    for each in win_coord:
+        # Если по всем трём индексам из любого выигрышного кортежа
+        # на игровом поле выпали одинаковые символы (X или O),
+        if board[each[0]] == board[each[1]] == board[each[2]]:
+            return board[each[0]]  # то возвращаем этот символ - он победил.
+    return False  # Иначе победа пока не достигнута
 
 
-# x_turn = input('Ход крестиков. Введите номер поля для Х: ')
-# print(x_turn)
+def main(board):  # Главная функция
+    counter = 0  # Счетчик ходов
+    win = False  # Изначально победителей нет
+    while not win:
+        draw_board(board)
+        if counter % 2 == 0:  # Если ход четный,
+            take_input("X")  # то ход "крестиков"
+        else:
+            take_input("O")  # Иначе ход "ноликов"
+        counter += 1
+        if counter > 4:  # Если хотя бы один сделал 3 хода
+            tmp = check_win(board)  # Проверяем на выигрыш
+            if tmp:  # в tmp символ победителя (X или O)
+                print(tmp, "выиграл!")
+                win = True
+                break
+        if counter == 9:  # Последний ход не может быть выигрышным
+            print("Ничья!")
+            break
+    draw_board(board)  # Рисуем поле завершённой игры
+
+
+main(board)  # Запуск игры
